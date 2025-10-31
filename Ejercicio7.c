cc572846e072618f90b15de9ed17fe73b8783d94
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 02:22:22 -0600

    Update Ejercicio7.c

--- Ejercicio7.c
+++ Ejercicio7.c
@@ -1,4 +1,32 @@
 /*Victor Seiji Nieto Morita
 clase 3 ejercicio 7*/
 
+#include <stdio.h>
 
+int main()
+{
+  int num1, num2, num3;
+
+  printf("Ingrese el primer número (una cifra): ");
+  scanf("%d", &num1);
+
+  printf("Ingrese el segundo número (una cifra): ");
+  scanf("%d", &num2);
+
+  printf("Ingresa el tercer número (una cifra): ");
+  scanf("%d", &num3);
+
+  if (num1 == 1 && num2 == 2 && num3 == 3)
+
+  {
+   printf("Acceso permitido\n");
+  }
+
+  else
+
+  {
+   printf("Acceso denegado\n");
+  }
+
+  return 0;
+}
