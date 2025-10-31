966d57b52f05887b0d4334c6468fedb2d41f0153
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 03:32:03 -0600

    Update Ejercicio9.c

--- Ejercicio9.c
+++ Ejercicio9.c
@@ -2,3 +2,50 @@
 clase 4 ejercicio 9*/
 
 
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+  int num;
+  int suma = 0, media = 0, mayor = 0, menor = 0;
+
+  do
+     {
+       printf("Dame el numero entero: ");
+       scanf("%d", &num);
+      }
+      
+ while(num < 0);
+ suma = num;
+ mayor = num;
+ menor = num;
+
+for(int i = 2; i <= 10; i++)
+  {
+       do
+      {
+       printf("Dame el numero entero: ");
+       scanf("%d", &num);
+      }
+      while(num < 0);
+    
+      suma = suma + num;
+      if(num > mayor)
+      {
+       mayor = num;
+      }
+      if(num < menor)
+      {
+        menor = num;
+      }
+ }
+ media = suma / 10;
+
+printf("La suma total de los numeros es: 5%d\n", suma);
+printf("El promedio total de los numeros es: %d\n", media);
+printf("El numero mayor es: %d\n", mayor);
+printf("El numero menor es: %d\n", menor);
+  
+     
+}
