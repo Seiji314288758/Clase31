5dae2f234d136481e73dcc701a2b8cd47d49a9b9
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 02:21:03 -0600

    Update Ejercicio6.c

--- Ejercicio6.c
+++ Ejercicio6.c
@@ -1,3 +1,24 @@
 /*Victor Seiji Nieto Morita
 clase 3 ejercicio 6*/
 
+#include <stdio.h>
+
+int main ()
+{
+  float x, fx;
+
+  printf("Ingrese el valor de x: ");
+  scanf("%f", &x);
+
+  if (x <= 0)
+   {
+    fx = x + 3;
+   }
+else 
+   {
+    fx = (x * x) + (2 * x);
+   }
+  printf("El valor de f(x) es: %.2f\n", fx);
+
+return 0;
+}
